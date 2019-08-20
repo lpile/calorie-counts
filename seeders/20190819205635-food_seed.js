e865d4073b733890eb1f6f4b7e8e14485cc4a524
lpile <logan.pile@gmail.com>
2019-08-20 14:36:52 -0600

    Change naming convention of foods to food

--- seeders/20190819205635-food_seed.js
+++ seeders/20190819205635-food_seed.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
-    return queryInterface.bulkInsert('Foods', [{
+    return queryInterface.bulkInsert('Food', [{
       id: 1,
       name: 'Banana',
       calories: 150,
@@ -28,6 +28,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Foods', null, {});
+    return queryInterface.bulkDelete('Food', null, {});
   }
 };
