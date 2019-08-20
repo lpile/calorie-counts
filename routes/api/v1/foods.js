273cb11898175a91c78f849297c26a2e37624d73
lpile <logan.pile@gmail.com>
2019-08-20 14:37:16 -0600

    Add foodsRouter and get all foods endpoint

---
+++ routes/api/v1/foods.js
@@ -0,0 +1,19 @@
+
+var express = require("express");
+var router = express.Router();
+var Food = require('../../../models').Food;
+
+/* GET all foods */
+router.get("/", function (req, res) {
+  Food.findAll()
+    .then(foods => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(foods));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error })
+    });
+});
+
+module.exports = router;
