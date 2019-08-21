1e730cdde55d61df12eadbed708711288d5a16f7
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-08-20 19:04:07 -0600

    Add spec for delete food route

--- app.spec.js
+++ app.spec.js
@@ -37,4 +37,18 @@ describe('api', () => {
       })
     });
   });
+	
+  describe('Test DELETE /api/v1/foods/:id path', () => {
+    test('should return a 204 status', () => {
+      return request(app).delete("/api/v1/foods/12").then(response => {
+        expect(response.status).toBe(204)
+      });
+    });
+    
+    test('should return a 404 for food not in the DB', () => {
+      return request(app).delete("/api/v1/foods/99").then(response => {
+				expect(response.status).toBe(404)
+      })
+    });
+  });
 });
