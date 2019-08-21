9eaa129b2c3914baac8080562514661f04dd9fe2
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-08-21 15:40:59 -0600

    Add post route and work on update route

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -2,6 +2,23 @@ var express = require('express');
 var router = express.Router();
 var Food = require('../../../models').Food;
 
+/*POST new food*/
+router.post("/", function(req, res) {
+  Food.create({
+          name: req.body.food.name,
+          calories: req.body.food.calories
+    })
+    .then(food => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(food));
+    })
+    .catch(error => {
+			console.log(req.body)
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
 /* GET all foods */
 router.get('/', function (req, res) {
   Food.findAll()
@@ -38,27 +55,64 @@ router.get('/:id', function (req, res) {
   });
 });
 
-/* Update a single food resource */
+///* Update a single food resource */
+//router.put('/:id', function (req, res) {
+//  Food.findOne({ where: { id: req.params.id }})
+//  .then(food => {
+//		if (!food) {
+//      res.setHeader('Content-Type', 'application/json');
+//      res.status(400).send(JSON.stringify({ error: 'Need Food Name And Calories' }));
+//		}
+//    if (!req.body.food.name && !req.body.food.calories) {
+//      res.setHeader('Content-Type', 'application/json');
+//      res.status(400).send(JSON.stringify({ error: 'Need Food Name And Calories' }));
+//    }  else if (!req.body.food.name && req.body.food.calories) {
+//      res.setHeader('Content-Type', 'application/json');
+//      res.status(400).send(JSON.stringify({ error: 'Need Food Name' }));
+//    } else if (!req.body.food.calories && req.body.food.name) {
+//      res.setHeader('Content-Type', 'application/json');
+//      res.status(400).send(JSON.stringify({ error: 'Need Food Calories' }));
+//    } else if (!food) {
+//      res.setHeader('Content-Type', 'application/json');
+//      res.status(404).send(JSON.stringify({ error: 'Food Not Found' }));
+//		}
+//    else {
+//      Food.update(
+//        {
+//          name: req.body.food.name,
+//          calories: req.body.food.calories
+//        },
+//        {
+//          returning: true,
+//          where: {
+//            id: parseInt(req.params.id)
+//          }
+//        }
+//      )
+//      .then(([rowsUpdate, [updatedFood]]) => {
+//        res.setHeader('Content-Type', 'application/json');
+//        res.status(202).send(JSON.stringify(updatedFood));
+//      })
+//      .catch(error => {
+//        res.setHeader('Content-Type', 'application/json');
+//        res.status(406).send({ error: 'Unable To Update' })
+//      });
+//    }
+//  })
+//  .catch(error => {
+//    res.setHeader('Content-Type', 'application/json');
+//    res.status(500).send({ error })
+//  });
+//});
+
 router.put('/:id', function (req, res) {
   Food.findOne({ where: { id: req.params.id }})
   .then(food => {
-    if (!req.body.name && !req.body.calories) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(400).send(JSON.stringify({ error: 'Need Food Name And Calories' }));
-    }  else if (!req.body.name && req.body.calories) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(400).send(JSON.stringify({ error: 'Need Food Name' }));
-    } else if (!req.body.calories && req.body.name) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(400).send(JSON.stringify({ error: 'Need Food Calories' }));
-    } else if (!food) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(404).send(JSON.stringify({ error: 'Food Not Found' }));
-    }else {
+		if (food) {
       Food.update(
         {
-          name: req.body.name,
-          calories: req.body.calories
+          name: req.body.food.name,
+          calories: req.body.food.calories
         },
         {
           returning: true,
@@ -75,13 +129,15 @@ router.put('/:id', function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         res.status(406).send({ error: 'Unable To Update' })
       });
-    }
-  })
-  .catch(error => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(500).send({ error })
-  });
+		}
+	})
+	.catch(error => {
+		res.setHeader('Content-Type', 'application/json');
+		res.status(400).send(JSON.stringify({ error: 'Need Food Name And Calories' }));
+	});
 });
+
+
 
 /* DELETE single food */
 router.delete('/:id', function(req, res) {
