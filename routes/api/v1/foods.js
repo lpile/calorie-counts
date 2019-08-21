4e74353ef90db62c4122009250d6f89af4237e3c
lpile <logan.pile@gmail.com>
2019-08-21 09:18:23 -0600

    Add edge cases for update single food route

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 var Food = require('../../../models').Food;
@@ -41,21 +40,42 @@ router.get('/:id', function (req, res) {
 
 /* Update a single food resource */
 router.put('/:id', function (req, res) {
-  Food.update(
-    {
-      name: req.body.name,
-      calories: req.body.calories
-    },
-    {
-      returning: true,
-      where: {
-        id: parseInt(req.params.id)
-      }
+  Food.findOne({ where: { id: req.params.id }})
+  .then(food => {
+    if (!food) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(404).send(JSON.stringify({ error: 'Food Not Found' }));
+    } else if (!req.body.name && !req.body.calories) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(400).send(JSON.stringify({ error: 'Need Food Name And Calories' }));
+    } else if (!req.body.name) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(400).send(JSON.stringify({ error: 'Need Food Name' }));
+    } else if (!req.body.calories) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(400).send(JSON.stringify({ error: 'Need Food Calories' }));
+    } else {
+      Food.update(
+        {
+          name: req.body.name,
+          calories: req.body.calories
+        },
+        {
+          returning: true,
+          where: {
+            id: parseInt(req.params.id)
+          }
+        }
+      )
+      .then(([rowsUpdate, [updatedFood]]) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(202).send(JSON.stringify(updatedFood));
+      })
+      .catch(error => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(406).send({ error: 'Unable To Update' })
+      });
     }
-  )
-  .then(([rowsUpdate, [updatedFood]]) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(202).send(JSON.stringify(updatedFood));
   })
   .catch(error => {
     res.setHeader('Content-Type', 'application/json');
--- tests/requests/update_food.spec.js
+++ tests/requests/update_food.spec.js
@@ -1,7 +1,6 @@
 var shell = require('shelljs');
 var request = require('supertest');
 var app = require('../../app');
-var Food = require('../../models').Food;
 
 describe('GET /api/v1/foods/:id path', () => {
   beforeAll(() => {
@@ -15,13 +14,50 @@ describe('GET /api/v1/foods/:id path', () => {
     shell.exec('npx sequelize db:migrate:undo:all')
   });
 
-  test('should return a 201 status', () => {
+  test('should return a 202 status', () => {
     return request(app).put('/api/v1/foods/1').send({
       name: 'Banana',
       calories: 140
     })
     .then(response => {
       expect(response.status).toBe(202)
+    });
+  });
+
+  test('should return a 400 status if no information is in request', () => {
+    return request(app).put('/api/v1/foods/1').then(response => {
+      expect(response.status).toBe(400)
+      expect(response.body.error).toBe('Need Food Name And Calories')
+    });
+  });
+
+  test('should return a 400 status if food name is not in request', () => {
+    return request(app).put('/api/v1/foods/1').send({
+      calories: 140
+    })
+    .then(response => {
+      expect(response.status).toBe(400)
+      expect(response.body.error).toBe('Need Food Name')
+    });
+  });
+
+  test('should return a 400 status if food calories is not in request', () => {
+    return request(app).put('/api/v1/foods/1').send({
+      name: 'Banana'
+    })
+    .then(response => {
+      expect(response.status).toBe(400)
+      expect(response.body.error).toBe('Need Food Calories')
+    });
+  });
+
+  test('should return a 404 status if food id is not in database', () => {
+    return request(app).put('/api/v1/foods/100').send({
+      name: 'Mint',
+      calories: 100
+    })
+    .then(response => {
+      expect(response.status).toBe(404)
     });
   });
 
