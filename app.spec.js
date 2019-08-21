89e9e6596d367b60b53ec05d64dfc6da87faf27b
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-08-21 11:48:43 -0600

    Normalize code with only single quotes, add extra error response assertation

--- app.spec.js
+++ app.spec.js
@@ -1,5 +1,5 @@
 var shell = require('shelljs');
-var request = require("supertest");
+var request = require('supertest');
 var app = require('./app');
 
 describe('api', () => {
@@ -16,7 +16,7 @@ describe('api', () => {
 
   describe('Test the root path', () => {
     test('should return a 200', () => {
-      return request(app).get("/").then(response => {
+      return request(app).get('/').then(response => {
         expect(response.statusCode).toBe(200)
       })
     });
@@ -24,13 +24,13 @@ describe('api', () => {
 
   describe('Test GET /api/v1/foods path', () => {
     test('should return a 200 status', () => {
-      return request(app).get("/api/v1/foods").then(response => {
+      return request(app).get('/api/v1/foods').then(response => {
         expect(response.status).toBe(200)
       });
     });
     
     test('should return an array of food objects', () => {
-      return request(app).get("/api/v1/foods").then(response => {
+      return request(app).get('/api/v1/foods').then(response => {
         expect(response.body.length).toEqual(3),
         expect(Object.keys(response.body[0])).toContain('name')
         expect(Object.keys(response.body[0])).toContain('calories')
@@ -40,14 +40,15 @@ describe('api', () => {
 	
   describe('Test DELETE /api/v1/foods/:id path', () => {
     test('should return a 204 status', () => {
-      return request(app).delete("/api/v1/foods/12").then(response => {
+      return request(app).delete('/api/v1/foods/12').then(response => {
         expect(response.status).toBe(204)
       });
     });
     
     test('should return a 404 for food not in the DB', () => {
-      return request(app).delete("/api/v1/foods/99").then(response => {
+      return request(app).delete('/api/v1/foods/99').then(response => {
 				expect(response.status).toBe(404)
+				expect(response.body.error).toBe('Not Found')
       })
     });
   });
