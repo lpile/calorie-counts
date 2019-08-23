3cb3769a93c9a5bfb5f3a27df809a6fd103a5ea0
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-08-23 12:42:54 -0600

    Add serializer skeleton for later use
    Co-authored-by: Logan Pile <25986836+lpile@users.noreply.github.com>

---
+++ serializers/meal.js
@@ -0,0 +1,6 @@
+//'use strict';
+//var JSONAPISerializer = require('jsonapi-serializer').Serializer;
+//
+//module.exports = new JSONAPISerializer('meals', {
+//  attributes: ['name', 'lastName']
+//});
