c4da1473b41078ee62c91255e2f49b21df06ef3a
lpile <logan.pile@gmail.com>
2019-08-23 22:29:09 -0600

    Add should delete a food test

--- tests/requests/foods/delete_food.spec.js
+++ tests/requests/foods/delete_food.spec.js
@@ -1,6 +1,7 @@
 var shell = require('shelljs');
 var request = require('supertest');
 var app = require('../../../app');
+var Food = require('../../../models').Food;
 
 describe('DELETE /api/v1/foods/:id path', () => {
 	beforeAll(() => {
@@ -18,9 +19,18 @@ describe('DELETE /api/v1/foods/:id path', () => {
     expect(response.statusCode).toBe(204);
   });
 
-	test('should return a 404 status if food is not in database', async () => {
+	test('should delete a food', async() => {
+    const response = await request(app).delete('/api/v1/foods/1');
+		let foods = await Food.findAll();
+		expect(foods.length).toBe(2);
+    expect(foods[0].name).not.toBe('Banana');
+    expect(foods[1].name).not.toBe('Banana');
+  });
+
+	test('should return a 404 status if food is not in database', async(done) => {
     const response = await request(app).delete('/api/v1/foods/100');
     expect(response.statusCode).toBe(404);
     expect(response.body.error).toBe('Food Not Found')
+		done();
   });
 });
